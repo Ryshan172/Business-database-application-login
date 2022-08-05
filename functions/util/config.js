b84fa0bca6430583643db6404f4435765dca7618
Ryshan Ramlall <ryshanramlall@outlook.com>
2022-08-05 14:02:24 +0200

    Database app functions

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyA6zUf_RvPPS4Hgjvy0N1r2aR3ciDHIPrM",
+  authDomain: "sustainatagapp.firebaseapp.com",
+  projectId: "sustainatagapp",
+  storageBucket: "sustainatagapp.appspot.com",
+  messagingSenderId: "417560713879",
+  appId: "1:417560713879:web:0cc8cad70a71ae8c10e7d2",
+  measurementId: "G-G6P414WZTS",
+};
